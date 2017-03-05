2c15e74eda017c5afc64fdfb053aaa8f97c69adc
Corin Alexandru <corin.alex@gmail.com>
2017-03-05 13:20:22 +0100

    Déplacement de la logique dans core.php Ajout de la possibilité de choisir le nombre de blocks à l'appel de la fonction Légère modification CSS Déplacement du code de génération rgb dans une fonction dédiée pour plus de flexibilité Petites optimisations Renommage du fichier index.phtml en index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once("core.php"); ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="master.css">
+        <meta charset="utf-8">
+        <title>V3</title>
+    </head>
+    <body>
+        <?= colorBlocks(50); ?>
+    </body>
+</html>
